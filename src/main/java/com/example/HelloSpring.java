15fd2eec567f340e28e6e62a4919bc95ec573119
java <java@bigdata>
2017-09-05 16:44:07 +0900

    DI Concept Study

--- src/main/java/com/example/HelloSpring.java
+++ src/main/java/com/example/HelloSpring.java
@@ -3,8 +3,13 @@ package com.example;
 public class HelloSpring {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		System.out.println("Spring의 두 가지 추상화된 설계");
+		System.out.println("1. DI(Dependency Injection): 의존 주입");
+		System.out.println("	- IoC의 철학을 Spring이 구현한 것");
+		//IoC(제어의 역전): 개발자의 간섭은 적게 하고, 자동화하자 - 소프트웨어 디자인 패턴(철학)
+		
+		System.out.println("2. AOP(Aspect Objected Programming): 관점 지향 프로그래밍");
 	}
 
 }
